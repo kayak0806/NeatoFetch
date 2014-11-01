9bcdf28d7204faf8db71396b606a05245b585a2e
vpreston <victorialynnpreston@gmail.com>
2014-11-01 00:11:02 -0400

    Using canny edges to ID the ball, works really well

--- HoughCirclesTests/hough_circle_vid_test.py
+++ HoughCirclesTests/hough_circle_vid_test.py
@@ -8,7 +8,7 @@ def plot_circles(img_src, img_out):
 	circles = cv2.HoughCircles(img_src, cv2.cv.CV_HOUGH_GRADIENT, 1, img_src.shape[0]/8, param1=10, param2=20, minRadius=20, maxRadius=30)
 
 	if circles is not None:
-		print 'Circles detected'
+		#print 'Circles detected'
 		for c in circles[0,:]:
 		    # draw the outer circle
 		    cv2.circle(img_out,(c[0],c[1]),c[2],(0,255,0),2)
@@ -39,20 +39,19 @@ while cap.isOpened():
 	ret, frame = cap.read()
 
 	# Processes video frames
-	blur = cv2.medianBlur(frame, 3)
-	grey = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+	#blur = cv2.medianBlur(frame, 3)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	edges = cv2.Canny(gray, 100, 150)
 
-	mask = color_shift(blur)
+	#mask = color_shift(blur)
 
-	plot_circles(mask, frame)
-
-	# Basic edge-finding using Canny detectionq
-	edges = cv2.Canny(mask, 60, 80)	
+	plot_circles(edges, frame)
+		
 
 	# Shows all of the things!
 	cv2.imshow('Video', frame)
-	cv2.imshow('Mask', mask)
-	# cv2.imshow('Canny', edges)
+	#cv2.imshow('Mask', mask)
+	cv2.imshow('Canny', edges)
 
 	if cv2.waitKey(50) & 0xFF == ord('q'):
 		break
@@ -61,7 +60,7 @@ if cv2.waitKey(50) & 0xFF == ord('q'):
 	cap.release()
 	cv2.destroyAllWindows()
 
-xlength, ylength = grey.shape
+xlength, ylength = gray.shape
 path = np.zeros((xlength, ylength, 3), np.uint8)
 for coor in circle_location:
 	cv2.circle(path,(coor), 5, (0,0,255), -1)
